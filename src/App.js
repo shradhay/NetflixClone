101734ef1f24ea7bae8ec05e477aba775839636f
shradhay <shraddhayannawar16@gmail.com>
2020-10-18 04:27:23 -0700

    Rows done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import request from "./request"
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
      <h1>NETFLIX</h1>
-     <Row title="Netflix Original" fetchURL={request.fetchNetflixOriginal}/>
+     <Row title="Netflix Original"
+     isLargeRow 
+     fetchURL={request.fetchNetflixOriginal}/>
      <Row title="Trending Now"  fetchURL={request.fetchTrending}/>
+     <Row title="Top Rated "  fetchURL={request.fetchToprated}/>
+     <Row title="Action Movies"  fetchURL={request.fetchActionMovies}/>
+     <Row title="Comedy Movies"  fetchURL={request.fetchComedyMovies}/>
+     <Row title="Horror movie"  fetchURL={request.fetchHorrorMovies}/>
+     <Row title="Romance Movies"  fetchURL={request.fetchRomanceMovies}/>
+     <Row title="Documentries"  fetchURL={request. fetchDocumentries}/>
      
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios"
 import "./Row.css"
 
 const base_url="https://image.tmdb.org/t/p/original"
-function Row({title,fetchURL}) {
+function Row({title,fetchURL,isLargeRow}) {
 
     const [movies,setMovies] = useState([]);
 
@@ -28,8 +28,10 @@ function Row({title,fetchURL}) {
                     return(
                     <img 
                     key={movie.id}
-                    className="row_poster"
-                    src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    src={`${base_url}${
+                        isLargeRow ?
+                        movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
 
                     )
                 })}
