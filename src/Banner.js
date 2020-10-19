51c7453e0990c67a5ce28bb8401ec48803452e5a
shradhay <shraddhayannawar16@gmail.com>
2020-10-18 22:33:44 -0700

    navbar and banner added

--- src/Banner.js
+++ src/Banner.js
@@ -31,6 +31,7 @@ function Banner() {
             backgroundSize:"cover",
             backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition:"center center",
+         
             }}
         >
         <div className="banner_content">
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
+import "./Nav.css"
 
 function Nav() {
+        const [show, handleShow] = useState()
+    useEffect(() => {
+       window.addEventListener("scroll",()=>{
+           if(window.scrollY > 100){
+               handleShow(true);
+           }
+           else
+           {
+               handleShow(false)
+           }
+       });
+       return ()=>{
+           window.removeEventListener("scroll");
+       }
+    }, [])
     return (
-        <div>
+        <div className = {`nav  ${show && "nav_black"}`}>
+            <img
+            className="navlogo"
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            />
+            <img/>
+
             
         </div>
     )
