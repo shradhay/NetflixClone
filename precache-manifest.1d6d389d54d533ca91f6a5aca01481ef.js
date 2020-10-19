fe55677a084764dec78464800bcd754df896bac9
shradhay <shraddhayannawar16@gmail.com>
2020-10-19 00:32:41 -0700

    Updates

---
+++ precache-manifest.1d6d389d54d533ca91f6a5aca01481ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a81dd8d7d297fd1280080f39b452b52",
+    "url": "/Netflixclone/index.html"
+  },
+  {
+    "revision": "eda90639bbbddd101d22",
+    "url": "/Netflixclone/static/css/main.259ac58d.chunk.css"
+  },
+  {
+    "revision": "d117ee1cd84ad840dc7a",
+    "url": "/Netflixclone/static/js/2.b8295ad3.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/Netflixclone/static/js/2.b8295ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda90639bbbddd101d22",
+    "url": "/Netflixclone/static/js/main.4e14afa4.chunk.js"
+  },
+  {
+    "revision": "549e34b45cf12617a337",
+    "url": "/Netflixclone/static/js/runtime-main.78c4daf4.js"
+  }
+]);
