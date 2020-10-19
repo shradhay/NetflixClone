212f612eec8a37aea47d94c77808a6173ae6ff04
shradhay <shraddhayannawar16@gmail.com>
2020-10-19 00:22:09 -0700

    onclick trailer added..

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,16 @@
 import React,{useState,useEffect} from 'react'
+
 import axios from "./axios"
 import "./Row.css"
+import YouTube from 'react-youtube';
+import movieTrailer from "movie-trailer"
+
 
 const base_url="https://image.tmdb.org/t/p/original"
 function Row({title,fetchURL,isLargeRow}) {
 
     const [movies,setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("")
 
     useEffect(()=>{
 
@@ -20,6 +25,29 @@ function Row({title,fetchURL,isLargeRow}) {
 
     },[fetchURL]);
     // console.log(movies)
+    const options={
+        height:"390",
+        width:"100%",
+        playersVars:{
+            //
+            autoplay:1,
+        }
+    }
+    const handleClick =(movie)=>{
+        if (trailerUrl) {
+            setTrailerUrl('')
+
+        }
+        else {
+            movieTrailer(movie?.name || "")
+            .then(url=>{
+                   const urlParams = new URLSearchParams( new URL(url).search);
+                   setTrailerUrl  ( urlParams.get('v'))
+            
+                })
+            .catch(error=> console.log(error))
+        }
+    }
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -27,17 +55,22 @@ function Row({title,fetchURL,isLargeRow}) {
                 {movies.map(movie =>{
                     return(
                     <img 
+                    onClick={()=> handleClick(movie)}
                     key={movie.id}
                     className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                     src={`${base_url}${
                         isLargeRow ?
-                        movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                        movie.poster_path : movie.poster_path}`} alt={movie.name} />
 
                     )
                 })}
 
             </div>
-            
+            {trailerUrl && <YouTube 
+            videoId={trailerUrl} 
+            opts={options}
+               />
+             }
             
             
         </div>
